28cd3e7a7dd0b3699c6b0a03599293aa8380ae5e
Nimira43 <n.rabone72@aol.com>
2025-02-10 16:03:11 +0000

    Checks if all required customer fields are present and valid:
    email: Must include '@'.
    
    name, street, postal-code, and city: Must not be null or empty.

--- backend/app.js
+++ backend/app.js
@@ -30,13 +30,32 @@ app.use((req, res) => {
 app.post('/orders', async (req, res) => {
   const orderData = req.body.order
 
-  if (orderData === null ||
+  if (
+    orderData === null ||
     orderData.items === null ||
     orderData.items.length === 0
   ) {
     return res
       .status(400)
       .json({ message: 'Missing Data.'})
+  }
+
+  if (
+    orderData.customer.email === null ||
+    !orderData.customer.email.includes('@') ||
+    orderData.customer.name === null ||
+    orderData.customer.name.trim() === '' ||
+    orderData.customer.street === null ||
+    orderData.customer.street.trim() === '' ||
+    orderData.customer['postal-code'] === null ||
+    orderData.customer['postal-code'].trim() === '' ||
+    orderData.customer.city === null ||
+    orderData.customer.city.trim() === ''
+  ) {
+    return res.status(400).json({
+      message:
+        'Ensure that all fields are filled in.'
+    })
   }
 })
 
