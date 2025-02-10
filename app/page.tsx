cc62b64597be344286190c3ca583a3513101ce36
Nimira43 <n.rabone72@aol.com>
2025-02-10 09:56:52 +0000

    Testing out Tailwind theme colours and new fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 export default function page() {
   return (
-    <div>CodeScribe</div>
+    <div className='p-10'>
+      <h1 className='text-primary text-4xl logo'>codeScribe</h1>
+      <hr />
+      <button className='my-10 px-3 py-1 rounded-sm uppercase text-2xl bg-primary-dark text-dark title'>Login</button>
+      <p className='text-primary'>Note taking markdown application for documentating your coding projects.</p>
+    </div>
   )
 }
