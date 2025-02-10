87111744404e480599b60d797076b2fd6687b83f
Nimira43 <n.rabone72@aol.com>
2025-02-10 15:42:07 +0000

    Setup early stages of server.

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,10 @@
 import fs from 'node:fs/promises'
 import bodyParser from 'body-parser'
 import express from 'express'
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.listen(3000)
